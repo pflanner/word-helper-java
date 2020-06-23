baafd827e2ab4a19b74b6d550efd5f4fcda2e8d8
Peter Flanner <peter.flanner@gmail.com>
2020-06-23 10:09:10 -0700

    Add a test case for max score

--- src/test/wordhelper/MainTest.java
+++ src/test/wordhelper/MainTest.java
@@ -1,11 +1,12 @@
 package wordhelper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
@@ -37,29 +38,64 @@ public class MainTest {
      */
     @Test
     public void maxScore() {
-        GameBoard board = Main.makeGameBoardFromFile("resources/easy-test.gbd");
-        List<Result> results = Main.computeHighestScore(board, "enaocgi", 0);
-        results.sort(Comparator.comparingInt(Result::getScore).reversed());
-        Result firstResult = results.get(0);
-
-        assertEquals(35, firstResult.getScore(), "max score calulation is not correct");
-        assertNotNull(firstResult.getTiles(), "we didn't get tiles for the first result");
-        assertNotEquals(0, firstResult.getTiles().size(), "first result tiles were empty");
-
-        Tiles expectedTiles = new Tiles();
-        expectedTiles.add(new Tile('c', new Location(2, 4)));
-        expectedTiles.add(new Tile('a', new Location(3, 4)));
-
-        Set<Integer> foundIndices = new HashSet<>(2);
-
-        for (Tile tile : firstResult.getTiles()) {
-            for (int i = 0; i < expectedTiles.size(); i++) {
-                if (tile.equals(expectedTiles.get(i))) {
-                    foundIndices.add(i);
-                }
+        class TestCase {
+            TestCase(String gameBoardPath, String rack, int expectedScore, Tiles expectedTiles) {
+                this.gameBoardPath = gameBoardPath;
+                this.rack = rack;
+                this.expectedScore = expectedScore;
+                this.expectedTiles = expectedTiles;
             }
+
+            final String gameBoardPath;
+            final String rack;
+            final int expectedScore;
+            final Tiles expectedTiles;
         }
 
-        assertEquals(expectedTiles.size(), foundIndices.size(), "computed tiles did not match expectation");
+        List<TestCase> testCases = new ArrayList<>();
+        testCases.add(new TestCase(
+                "resources/easy-test.gbd",
+                "enaogci",
+                35,
+                getTilesFor(
+                        new Tile('c', new Location(2, 4)),
+                        new Tile('a', new Location(3, 4))
+                )
+        ));
+        testCases.add(new TestCase(
+                "resources/easy-test2.gbd",
+                "jasiwpv",
+                50,
+                getTilesFor(
+                        new Tile('j', new Location(0, 1)),
+                        new Tile('a', new Location(0, 2))
+                )
+        ));
+
+        for (TestCase tc : testCases) {
+            GameBoard board = Main.makeGameBoardFromFile(tc.gameBoardPath);
+            List<Result> results = Main.computeHighestScore(board, tc.rack, 0);
+            results.sort(Comparator.comparingInt(Result::getScore).reversed());
+            Result firstResult = results.get(0);
+
+            assertEquals(tc.expectedScore, firstResult.getScore(), "max score calulation is not correct");
+            assertNotNull(firstResult.getTiles(), "we didn't get tiles for the first result");
+
+            Set<Integer> foundIndices = new HashSet<>(2);
+
+            for (Tile tile : firstResult.getTiles()) {
+                for (int i = 0; i < tc.expectedTiles.size(); i++) {
+                    if (tile.equals(tc.expectedTiles.get(i))) {
+                        foundIndices.add(i);
+                    }
+                }
+            }
+
+            assertEquals(tc.expectedTiles.size(), foundIndices.size(), "computed tiles did not match expectation");
+        }
+    }
+
+    private Tiles getTilesFor(Tile... tiles) {
+        return new Tiles(Arrays.asList(tiles));
     }
 }
